fe21b192ce2cb156250d5c0f289c46b36eb2fe5d
Matěj Beran <matej.beran96@gmail.com>
2019-09-14 20:52:08 +0200

    Create splash scene and level loader.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int splashScreenLoadTime = 3;
+
+    int currentSceneIndex;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            LoadStartSceneFromSplashScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    private IEnumerator DelayNextScene(int sceneNumber, int seconds = 0)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    private IEnumerator DelayNextScene(string sceneName, int seconds = 0)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void LoadStartSceneFromSplashScene()
+    {
+        StartCoroutine(DelayNextScene(1, splashScreenLoadTime));
+    }
+}
