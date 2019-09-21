fc22ef5c2da82f758c873a44aaa45b5d8dd5b7fb
Matěj Beran <matej.beran96@gmail.com>
2019-09-21 17:04:23 +0200

    Check if attacker is in the same lane as denfender and change animation accordingly.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,10 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
+
+    private AttackerSpawner myLaneSpawner;
+    private Animator animator;
+
+    void Start()
+    {
+        SetLaneSpawner();
+        animator = GetComponent<Animator>();
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in attackerSpawners)
+        {
+            bool isCloseEnough = Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon;
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    void Update()
+    {
+        animator.SetBool("IsAttacking", IsAttackerInLane());
+    }
+
+    private bool IsAttackerInLane()
+    {
+        return myLaneSpawner.transform.childCount > 0;
+    }
 
     public void Fire()
     {
