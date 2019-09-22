d6d34ba5be78b2b2814eb3b99702fe9d4e04f452
Matěj Beran <matej.beran96@gmail.com>
2019-09-22 13:07:09 +0200

    Add Fox attacker.

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var otherGameObject = other.gameObject;
+
+        var defender = otherGameObject.GetComponent<Defender>();
+        var gravestone = otherGameObject.GetComponent<Gravestone>();
+
+        if (gravestone)
+        {
+            animator.SetTrigger("JumpTrigger");
+        }
+
+        else if (defender)
+        {
+            GetComponent<Attacker>().Attack(defender);
+        }
+    }
+}
---
+++ Assets/Scripts/Gravestone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravestone : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Attacker attacker = other.gameObject.GetComponent<Attacker>();
+
+        if (attacker)
+        {
+            // TODO: do animation
+        }
+    }
+}
