7dadca21aa3f694b3fe304379ccef7c2d2574497
Matěj Beran <matej.beran96@gmail.com>
2019-09-24 18:33:45 +0200

    Add cost label to buttons.

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -1,10 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour
 {
     [SerializeField] Defender defenderPrefab;
+
+    void Start()
+    {
+        LabelButtonWithCost();
+    }
+
+    private void LabelButtonWithCost()
+    {
+        Text costText = GetComponentInChildren<Text>();
+        if (costText)
+        {
+            costText.text = defenderPrefab.GetStarCost().ToString();
+        }
+        else
+        {
+            Debug.LogError(name + " has no cost text, add some!");
+        }
+    }
 
     void OnMouseDown()
     {
